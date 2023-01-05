a3063c8b29be62bf84c4ca0fbcb73038f758ca45
Elvan H <elvanhd@gmail.com>
2023-01-06 00:11:50 +0700

    creating items in each store

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ stores = [
 
 @app.get('/store')
 def list_stores():
+    # List all the stores
     return {
         'stores': stores
     }
@@ -28,6 +29,8 @@ def list_stores():
 
 @app.post('/store')
 def create_store():
+    # Create a new store
+
     request_data = request.get_json()
 
     new_store = {}
@@ -44,3 +47,33 @@ def create_store():
         'message': 'Store created',
         'store': new_store
     }, 201
+
+
+@app.post('/store/<string:name>/item')
+def create_item_in_store(name):
+    # Create items in each store
+
+    request_data = request.get_json()
+
+    for store in stores:
+        if store['name'] == name:
+            if request_data is None:
+                return {
+                    'message': 'Item not created, request_data is None'
+                }, 400
+
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+
+            store['items'].append(new_item)
+
+            return {
+                'message': 'Item created',
+                'item': new_item
+            }, 201
+
+    return {
+        'message': 'Store not found'
+    }, 404
