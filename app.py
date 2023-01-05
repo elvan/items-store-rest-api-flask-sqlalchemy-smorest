8813cdf68542390a8b098dfb378b5cc8f9f67309
Elvan H <elvanhd@gmail.com>
2023-01-05 23:57:36 +0700

    creating stores in the rest-api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -20,7 +20,27 @@ stores = [
 
 
 @app.get('/store')
-def get_stores():
+def list_stores():
     return {
         'stores': stores
     }
+
+
+@app.post('/store')
+def create_store():
+    request_data = request.get_json()
+
+    new_store = {}
+
+    if request_data is not None:
+        new_store = {
+            'name': request_data['name'],
+            'items': []
+        }
+
+        stores.append(new_store)
+
+    return {
+        'message': 'Store created',
+        'store': new_store
+    }, 201
