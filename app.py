1dd9142c3bb98fe0ddac372376465e63ff433016
Elvan Hidayat <elvanhd@gmail.com>
2023-05-14 13:40:24 +0700

    update and delete items and stores

--- app.py
+++ app.py
@@ -42,6 +42,32 @@ def create_item():
     return item
 
 
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
+        )
+    try:
+        item = items[item_id]
+        item |= item_data
+
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
 @app.get("/item")
 def get_all_items():
     return {"items": list(items.values())}
@@ -72,6 +98,15 @@ def create_store():
     stores[store_id] = store
 
     return store
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
 
 
 @app.get("/store")
