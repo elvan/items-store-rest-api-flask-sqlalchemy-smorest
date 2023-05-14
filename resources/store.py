a8e73313f03da9e5794a6f019e09f061794df815
Elvan Hidayat <elvanhd@gmail.com>
2023-05-14 17:21:16 +0700

    create sqlalchemy models

--- resources/store.py
+++ resources/store.py
@@ -1,10 +1,7 @@
-import uuid
-
 from flask.views import MethodView
-from flask_smorest import Blueprint, abort
+from flask_smorest import Blueprint
 
-from db import stores
-from schemas import StoreSchema
+from schemas import ItemSchema, StoreSchema
 
 blp = Blueprint("Stores", "stores", description="Operations on stores")
 
@@ -12,35 +9,20 @@ blp = Blueprint("Stores", "stores", description="Operations on stores")
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
-    def get(cls, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found.")
+    def get(self, store_id):
+        raise NotImplementedError("Getting a store is not implemented.")
 
-    def delete(cls, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted."}
-        except KeyError:
-            abort(404, message="Store not found.")
+    def delete(self, store_id):
+        raise NotImplementedError("Deleting a store is not implemented.")
 
 
 @blp.route("/store")
 class StoreList(MethodView):
-    @blp.response(200, StoreSchema(many=True))
-    def get(cls):
-        return stores.values()
+    @blp.response(200, ItemSchema(many=True))
+    def get(self):
+        raise NotImplementedError("Listing stores is not implemented.")
 
     @blp.arguments(StoreSchema)
     @blp.response(201, StoreSchema)
-    def post(cls, store_data):
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message=f"Store already exists.")
-
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
-
-        return store
+    def post(self, store_data):
+        raise NotImplementedError("Creating a store is not implemented.")
